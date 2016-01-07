d2960ff153797c1dbf66fe0dbbd5f8c6711034c9
Gemisheresy <Gemisheresy@gmail.com>
2016-01-07 16:31:22 -0500

    Added the ability to search spells and items.

--- app.js
+++ app.js
@@ -3,24 +3,53 @@ var keyFile = require('./keyFile');
 var request = require('request');
 var app = express();
 var apiKey = keyFile.apiKey;
-var weapon;
-var header;
-
-app.param('id',function(req,res,next,value){
+var output;
+var serach;
+/*
+app.param('itemid',function(req,res,next,value){
   console.log(value);
   request('https://us.api.battle.net/wow/item/'+value+'?locale=en_US&apikey='+apiKey,function(response,data){
-    weapon = data.body;
+    output = data.body;
     console.log(data.headers);
-    console.log(weapon);
-    res.write('<p>' +toString(weapon) + '</p>');
-    
+    console.log(output);
+    });
+  next();
+})
+app.get('/item/:itemid',function(req,res,next){
+  console.log('Item Requested');
+
+});
+app.param('spellid',function(req,res,next,value){
+  console.log(value);
+  request('https://us.api.battle.net/wow/spell/'+value+'?locale=en_US&apikey='+apiKey,function(response,data){
+    output = data.body;
+    console.log(data.headers);
+    console.log(output);
+    });
+  next();
+})
+app.get('/spell/:spellid',function(req,res,next){
+  console.log("Spell Requested");
+})
+*/
+app.param('type',function(req,res,next,type){
+  search = type;
+  console.log(search);
+  next();
+})
+app.param('id',function(req,res,next,value){
+  console.log(search,value);
+  request('https://us.api.battle.net/wow/'+search+ '/' +value+'?locale=en_US&apikey='+apiKey,function(response,data){
+    output = data.body;
+    console.log(data.headers);
+    console.log(output);
   });
   next();
 })
-app.get('/item/:id',function(req,res,next){
-  console.log('You have a hit');
-
+app.get('/index/:type/:id',function(req,res,next){
+  console.log('Hit me');
 });
+
 
 var server = app.listen(8080,function(){
   var host= server.address().address;
